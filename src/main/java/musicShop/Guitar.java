b93ca003ec6e769fa76c45a7f154cab35f8a2241
Simon <simonwatson176@gmail.com>
2021-09-03 20:34:09 +0100

    guitar glass & test added & 1 test ran

--- src/main/java/musicShop/Guitar.java
+++ src/main/java/musicShop/Guitar.java
@@ -1,4 +1,44 @@
 package musicShop;
 
-public class Guitar {
+import Behaviours.IPlay;
+import Behaviours.ISell;
+
+public class Guitar extends Instrument implements IPlay, ISell {
+    private String model;
+    private int numberOfStrings;
+    private int costPrice;
+    private int listPrice;
+
+    public Guitar(String model, String colour, int numberOfStrings, int costPrice, int listPrice) {
+        super(colour, "String");
+        this.numberOfStrings = numberOfStrings;
+        this.model = model;
+        this.costPrice = costPrice;
+        this.listPrice = listPrice;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public int getNumberOfStrings() {
+        return this.numberOfStrings;
+    }
+
+    public int getCostPrice() {
+        return this.costPrice;
+    }
+
+    public int getListPricePrice() {
+        return this.listPrice;
+    }
+
+    public String play() {
+        return "Instrument noises";
+    }
+
+    public int calculateMarkup() {
+        return this.listPrice - this.costPrice;
+    }
+
 }
