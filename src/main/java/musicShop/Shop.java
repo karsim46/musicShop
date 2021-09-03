cd89408859ea56df08597fb0a3ea70fd7fc75711
Simon <simonwatson176@gmail.com>
2021-09-03 21:39:21 +0100

    added guitar to shop & tested

--- src/main/java/musicShop/Shop.java
+++ src/main/java/musicShop/Shop.java
@@ -1,4 +1,32 @@
 package musicShop;
 
+import Behaviours.ISell;
+
+
+import java.util.ArrayList;
+
 public class Shop {
+    private String name;
+    private ArrayList<ISell> stock;
+
+    public Shop(String name) {
+        this.name = name;
+        this.stock = new ArrayList<ISell>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int stockCount() {
+        return this.stock.size();
+    }
+
+    public void addToStock(ISell item) {
+        this.stock.add(item);
+    }
+
+    public void removeFromStock(ISell item) {
+        this.stock.remove(item);
+    }
 }
